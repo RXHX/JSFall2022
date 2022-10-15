23c65807ff250b894ccc9e4d7a269cc50d017cfc
Kediyal <kediyalr@student.douglascollege.ca>
2022-10-14 20:05:39 -0700

    css fixed

--- Assignments/Project Overview for Nodejs Express/app.js
+++ Assignments/Project Overview for Nodejs Express/app.js
@@ -1,13 +1,12 @@
 const express= require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-app.use('static',express.static('public'));
+app.use('/static',express.static('public'));
 app.set('view engine','pug');
 const port = 3000;
-var rightNow = " ";
+var myName = "Rohan Kediyal";
 app.get('/',(req,res)=>{
-    res.render('project',{rightNow});
+    res.render('layout',{myName});
 });
 app.listen(port,(req,res)=>{
     console.log(`The app is running at ${port}`);
