9efd45c5e35818bfaf20299b30a9ea6346a22840
RXHX <rohan.kediyal@gmail.com>
2022-10-16 01:17:06 -0700

    image problem and calling from pug file remaining

--- Assignments/Project Overview for Nodejs Express/app.js
+++ Assignments/Project Overview for Nodejs Express/app.js
@@ -2,7 +2,6 @@ const express= require('express');
 const data = require('./data.json');
 const app = express();
 const bodyParser = require('body-parser');
-const e = require('express');
 app.use('/static',express.static('public'));
 app.set('view engine','pug');
 const port = 3000;
@@ -13,75 +12,49 @@ const information = JSON.parse(JSON.stringify(data));
 const project = information.project;
 const about = information.about;
 const index = information.index;
-const layout = information.layout;
 const router = information.router;
 const templatePage = information.constants;
 
 
-// app.get(router.project,(req,res)=>{      
-           
-             
-//     res.render(templatePage.project, {
-//         projectname: project[i].project_name,
-//         description:project[i].description,
-//         technologies:project[i].technologies,
-//         imageurls:project[i].image_urls
-//     });
-
-// });
- 
-var temp = "Hello";
-
-  for(var i=0; i<project.length;i++)
-  {
-  
-        app.get(router.project,(req,res)=>{      
-              
-
-           // if(i == project.length)
-           // {
-              //  console.log("Value of i is :"+i);
-            //    res.end();
-          //  }
-          //  else{
-                res.render(templatePage.project, {projectname: project[i].project_name,
-                    description:project[i].description,
-                    technologies:project[i].technologies,
-                    imageurls:project[i].image_urls});
-           // }
-                
-                }   
-                );
-
-    
-
-      }
 
 
-   
-   
-  
-  
+function checker(info)
+{
+    console.log("Information is: "+info);
+}
 
- 
 
-app.get(router.layout,(req,res)=>{
-    res.render(templatePage.layout,{
-        name: layout.user.name , 
-        description1:layout.user.description1, 
-        description2:layout.user.description2, 
-        profilePic:layout.user.profilePic,
-        url:router.about
+ app.get(router.project,(req,res)=>{
+  res.render(templatePage.project,{
+    a:project.project_name,
+    b:project.description,
+    c:project.technologies,
+    d:project.image_urls
+  });
+ });
+
+app.get(router.index,(req,res)=>{
+    res.render(templatePage.index,{
+        name: index.user.name , 
+        description1:index.user.description1, 
+        description2:index.user.description2, 
+        profilePic:index.user.profilePic,
+        about:router.about,
+        projectHeading:index.user.projectHeading,
+        Portfolio:index.user.Portfolio,
+        list:project,
+        project:router.project
+
     });
 });
 
 app.get(router.about,(req,res)=>{
     
     res.render(templatePage.about,{
-        name:layout.user.name,
+        name:index.user.name,
         title:about.resume.title,
-        description1:layout.user.description1, 
-        description2:layout.user.description2,
+        description1:index.user.description1, 
+        description2:index.user.description2,
         generalDescription:about.resume.generalDescription,
         projectExperience:about.resume.projectExperience,
         valueAddonToOrganization:about.resume.valueAddonToOrganization,
@@ -100,9 +73,6 @@ app.get(router.about,(req,res)=>{
     });
 });
 
-app.get(router.index,(req,res)=>{
-    res.render(templatePage.index,{});
-});
 
 
 app.listen(port,(req,res)=>{
