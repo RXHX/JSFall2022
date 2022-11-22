d0c44395ec297f273575cb029784887e253a8a69
Kediyal <kediyalr@student.douglascollege.ca>
2022-11-21 17:32:43 -0800

    CORS problem Solved

--- Project/client/src/index.js
+++ Project/client/src/index.js
@@ -1,7 +1,32 @@
 import React,{useEffect,useState} from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-const Practice4 = () => {
+
+
+
+const DisplayPersonality = (props) => {
+  return(
+<div>
+<div className="card">
+    <img src={props.image} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">{props.quote}</h5>
+      <p className="card-text">{props.source}</p>
+      <p className="card-text">{props.citation}</p>
+      <p className="card-text">{props.year}</p>
+      <a href="#" className="btn btn-primary">Click Here</a>
+    </div>
+  </div>
+  </div>
+  )
+   
+
+}
+
+
+
+
+const App = () => {
     /*
      * 1. Pull the fetch call out into it's own function fetchCat
      * 2. Call fetchCat on initial page load as done currently
@@ -9,28 +34,32 @@ const Practice4 = () => {
      * 4. Make sure to cancel the timer when component is unmounted
      */
   
-    const [catImg, setCatImg] = useState(``);
+    const [data, setData] = useState([]);
   
-    const baseUrl = "http://localhost:3000/randomData";
+    const baseUrl = "http://localhost:3001/randomData";
     useEffect(() => {
-        axios({
-            method: 'get',
-            url: baseUrl,
-            withCredentials: false
-          }).then((response) => {
-        console.log(response)
-        setCatImg(response);
-      });
+       axios.get(baseUrl).then(response => setData(response.data))
+       .catch(error => console.error(error));
     }, []);
      
     return (
       <>
         <h1>Random Cat Photo</h1>
-        
-        {console.log(catImg)}
-      </>
+        {console.log(data)}
+        {data.map((personality) =>  
+               <DisplayPersonality
+                  quote = {personality.quote}
+                  source = {personality.source}
+                  citation = {personality.citation}
+                  image = {personality.image}
+                  year = {personality.year}
+                  key = {personality.id.toString()}                      
+               />
+           )
+           }
+             </>
     );
   };
 
 
-  ReactDOM.render(<Practice4 />, document.getElementById("root"));
+  ReactDOM.render(<App />, document.getElementById("root"));
