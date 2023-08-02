00dcdf23a8aa9de6c83abbb25d92dc905b7ceb3d
Kediyal <kediyalr@student.douglascollege.ca>
2023-08-02 15:14:27 -0700

    Extra Folder added

--- quiz1/app.js
+++ quiz1/app.js
@@ -8,36 +8,39 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 });
-var data= '';
 
-app.post('/',(req,res)=>{
-    add.push(req.body.name);
-    add.push(req.body.age);
+
+
+//var data= '';
+
+// app.post('/',(req,res)=>{
+//     add.push(req.body.name);
+//     add.push(req.body.age);
    
-    for(var i=0;i<add.length;i++)
-    {
-        data += `${add[i]}<br>`;
-    }
+//     for(var i=0;i<add.length;i++)
+//     {
+//         data += `${add[i]}<br>`;
+//     }
 
 
-   res.send(`
-   <!DOCTYPE html>
-<head>
-    <title>Web Page</title>
-</head>
-<body>
-<form action="/" method="post">
-<label >Name:</label>   
-<input name="name" type="text"></input><br>
-<label>Age</label>
-<input name="age" type="number"></input><br>
-<input type="submit"></input>
-</form>
-${data}
-</body>
-</html>`)
+//    res.send(`
+//    <!DOCTYPE html>
+// <head>
+//     <title>Web Page</title>
+// </head>
+// <body>
+// <form action="/" method="post">
+// <label >Name:</label>   
+// <input name="name" type="text"></input><br>
+// <label>Age</label>
+// <input name="age" type="number"></input><br>
+// <input type="submit"></input>
+// </form>
+// ${data}
+// </body>
+// </html>`)
      
-});
+// });
 
 
 
